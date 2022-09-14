4d1d6ba6b1dd63f08e9db7dd2a060a3e6336bb17
akrishnan <anikrishnantango@gmail.com>
2022-09-14 13:59:25 +0100

    add playerPaddle on mousemove

--- Paddle.js
+++ Paddle.js
@@ -1,5 +1,13 @@
 export default class Paddle {
     constructor(paddleElem) {
-        this.paddleElem = paddleElem
+        this.paddleElem = paddleElem 
+    }
+
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"))
+    }
+
+    set position(value) {
+        this.paddleElem.style.setProperty("--position", value)
     }
 }
