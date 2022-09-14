bfa3898f606f7aa72381d3f1d7b73c29c1438ae2
akrishnan <anikrishnantango@gmail.com>
2022-09-14 12:16:47 +0100

    add time loop and working ball update

--- Ball.js
+++ Ball.js
@@ -3,10 +3,17 @@ export default class Ball {
         this.ballElem = ballElem
     }
 
-    
+    get x () {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
 
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value)
+    }
 
-
+    update(delta) {
+        this.x = 5
+    }
 
 }
 
