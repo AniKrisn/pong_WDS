7ac6bc0c794c9352fac6619413c759f7d9059aa0
akrishnan <anikrishnantango@gmail.com>
2022-09-14 12:40:28 +0100

    add reset but random direction broken

--- Ball.js
+++ Ball.js
@@ -1,6 +1,9 @@
+const INITIAL_VELOCITY = 0.08
+
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
+        this.reset()
     }
 
     get x () {
@@ -11,9 +14,37 @@ export default class Ball {
         this.ballElem.style.setProperty("--x", value)
     }
 
-    update(delta) {
-        this.x = 5
+    get y () {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
     }
+
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value)
+    }
+
+    reset() {
+        this.x = 50
+        this.y = 50
+        this.direction = { x: 0.75, y: 0.5 }
+        while (Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9) {
+            const heading = randomNumBetween(0, 2 * Math.PI)
+            this.direction = {x: Math.cos(heading), y: Math.sin(heading) }
+        }
+        this.velocity = INITIAL_VELOCITY
+    }
+
+
+
+
+    update(delta) {
+        this.x += this.direction.x * this.velocity
+        this.y += this.direction.y * this.velocity
+    }
+
+
 
 }
 
+function randomNumBetween(min,max) {
+    return Math.random() * (max - min) + min
+}
